4bc8b48e075c6d384fa5f3c72c4314be0cc2971b
Gleb E <gleb.e.learn@gmail.com>
2023-02-05 18:47:50 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,23 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let decoded = '';
+    for (let i=0; i<expr.length; i += 10) { 
+        const letterCodePadded = expr.slice(i, i + 10);
+        console.log(letterCodePadded);
+        if (letterCodePadded === '**********') {
+          decoded = decoded + ' ';
+        } else {
+        let morseCode = '';
+        const letterCode = letterCodePadded.replace(/0*/, '');
+        for (let j=0; j < letterCode.length; j += 2) {
+          const symbol = +letterCode.slice(j, j+2);
+          (symbol === 10) ? morseCode = morseCode + '.' : morseCode = morseCode + '-';
+          }
+        decoded = decoded + MORSE_TABLE[morseCode];
+        }       
+    }
+    return decoded;
 }
 
 module.exports = {
